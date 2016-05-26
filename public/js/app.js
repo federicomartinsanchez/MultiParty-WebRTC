d1b70e3808b6612d93a4d47b6f3e72c8376853a1
Fede <fedekite1@gmail.com>
2016-05-26 21:53:33 +0200

    asdasd

--- public/js/app.js
+++ public/js/app.js
@@ -127,7 +127,13 @@ socket.on("info", function(message) {
 
 ///////**************************  RTCPEERCONNECTION  **************************///////
 
-var pc_config = { 'iceServers': [{ 'url': 'stun:stun.stunprotocol.org' }] };
+var pc_config = { 'iceServers': [{ 
+'url': 'stun:stun.l.google.com:19302',
+'url': 'stun:stun1.l.google.com:19302',
+'url': 'stun:stun2.l.google.com:19302',
+'url': 'stun:stun3.l.google.com:19302',
+'url': 'stun:stun4.l.google.com:19302'
+ }] };
 var pc_constraints = {
     'optional': [
         { 'DtlsSrtpKeyAgreement': true },
